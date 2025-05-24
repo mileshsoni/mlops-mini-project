47052cb4604372cd33016f03d4d17af41cb89c06
mileshsoni <95475731+mileshsoni@users.noreply.github.com>
2025-05-24 14:51:01 +0530

    add model promotion code

---
+++ scripts/promote_model.py
@@ -0,0 +1,41 @@
+# promote model
+
+import os
+import mlflow
+
+def promote_model():
+    # Set up DagsHub credentials for MLflow tracking
+    dagshub_token = os.getenv("DAGSHUB_PAT")
+    if not dagshub_token:
+        raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    dagshub_url = "https://dagshub.com"
+    repo_owner = "mileshsoni"
+    repo_name = "mlops-mini-project"
+
+    # Set up MLflow tracking URI
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+    client = mlflow.MlflowClient()
+
+    model_name = "my_model"
+    
+    # get the latest version in production and move it to archived
+    try:
+        latest_mv = client.get_latest_versions(model_name, stages=['Production'])[0]
+        client.set_registered_model_alias(model_name, 'Archived', latest_mv.version)
+    except:
+        pass
+    
+    # Get the latest version in staging
+    latest_version_staging = client.get_model_version_by_alias(name="MyModel", alias="staging").version
+    
+    client.set_registered_model_alias(model_name, 'Production', latest_version_staging)
+    
+    print(f"Model version {latest_version_staging} promoted to Production")
+
+if __name__ == "__main__":
+    promote_model()
